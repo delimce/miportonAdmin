ccc49193ace1e8551129730d342b58169f32787e
luis D' Lima <delimce@gmail.com>
2013-07-24 14:58:32 -0430

    ajustes propiedades del framework en miporton

--- app/controllers/main/index.php
+++ app/controllers/main/index.php
@@ -1,1 +1,15 @@
 <?php
+
+function _index() {
+
+    Security::sessionActive();
+
+    
+     if(!Security::isSessionActive())
+         Front::redirect ("main/login");
+
+
+    $data['siteTitle'] = 'Mi Porton [Administración]';
+    $data['body'][] = View::do_fetch(VIEW_PATH . 'main/index_view.php');
+    View::do_dump(LAYOUT_PATH . 'layout1.php', $data);
+}
