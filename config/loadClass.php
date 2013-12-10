b9fed18892b82c9b05cc1e82fc2d1351ab61fa24
ldeli_000 <ldeli_000@LDELIMA-PC>
2013-12-10 14:56:16 -0430

    correcciones en el framework Avalon

--- config/loadClass.php
+++ config/loadClass.php
@@ -2,13 +2,13 @@
 //llama dinamicamente a todas las clases siempre y cuando tengan el mismo nombre del archivo .php para php5
 //carga las clases de la carpeta core y vendor, las core son las clases hechas por el autor del framework
 
-function __autoload($class) {
-    
-     $path =  "/../lib/core/";
-         
-     require_once(__DIR__ . $path . $class . '.php');
-     
-    
-}
+
+
+
+spl_autoload_register(function ($class) {
+ 	$path =  "/../lib/core/";
+    include (__DIR__ . $path . $class . '.php');
+});
+
 
 ?>
